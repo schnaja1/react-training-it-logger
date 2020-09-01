f220465a3863c4c531fd8d144396ac9d7ac60b63
Jaroslav Schnaubert <jardaschnaubert@seznam.cz>
2020-09-01 10:25:38 +0200

    Project Start - Components & UI

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preloader = () => {
+  return (
+    <div className="progress blue ligten-4">
+      <div className="indetermined blue"/>
+    </div>
+  )
+}
+
+export default Preloader
